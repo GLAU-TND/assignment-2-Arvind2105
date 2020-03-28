3baf31469ba38466a0fbd220c6580e5d0b98fd06
Arvind2105 <arvind.tomar_cs18@gla.ac.in>
2020-03-28 18:58:52 +0530

    made a array list of myBinarySearchTree then add 10 random values then print their values

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,24 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<Integer>();
+        myBinarySearchTree.add(10);
+        myBinarySearchTree.add(20);
+        myBinarySearchTree.add(30);
+        myBinarySearchTree.add(1);
+        myBinarySearchTree.add(2);
+        myBinarySearchTree.add(3);
+        myBinarySearchTree.add(4);
+        myBinarySearchTree.add(18);
+        myBinarySearchTree.add(19);
+        myBinarySearchTree.add(50);
+        myBinarySearchTree.print();
+        System.out.println("number of nodes who don't have their left child" + myBinarySearchTree.numberOFLeftChild);
+
+
+    }
 }
