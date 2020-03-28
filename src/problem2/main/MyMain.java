de90a8dd97fbb10b141af1f12d2b908fb183a923
Arvind2105 <arvind.tomar_cs18@gla.ac.in>
2020-03-28 19:44:38 +0530

    made an array list of MyBinarySearchTree and add seven values..

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        myBinarySearchTree.add(10);
+        myBinarySearchTree.add(2);
+        myBinarySearchTree.add(3);
+        myBinarySearchTree.add(5);
+        myBinarySearchTree.add(6);
+        myBinarySearchTree.add(8);
+        myBinarySearchTree.add(20);
+
+    }
 }
